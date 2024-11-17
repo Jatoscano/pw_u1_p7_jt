79dce39db9283034dfd3dbf52b41e36ae39cad1f
Jatoscano <jatoscano@uce.edu.ec>
2024-11-17 18:13:09 -0500

    Taller 8: Desestructuracion de Arreglos y Objetos

--- js/jsexternolambda.js
+++ js/jsexternolambda.js
@@ -154,3 +154,187 @@ const conceptosJS = () => {
         console.log("No son lo mismo");
     }
 }
+
+//Arreglo de Objetos
+const est1 = {
+    nombre: 'Juan',
+    apellido: 'Toscano',
+    edad: 28,
+    genero: 'Masculino',
+    ciudadNacimiento: 'Quito'
+}
+
+const est2 = {
+    nombre: 'Maria',
+    apellido: 'Perez',
+    edad: 25,
+    genero: 'Femenino',
+    ciudadNacimiento: 'Guayaquil'
+}
+
+const est3 = {
+    nombre: 'Pedro',
+    apellido: 'Alvarez',
+    edad: 30,
+    genero: 'Masculino',
+    ciudadNacimiento: 'Cuenca'
+}
+const estudiantes1 = [est1, est2, est3]
+
+console.log(estudiantes1);
+console.log(estudiantes1[0]);
+console.log(estudiantes1[1].nombre);
+
+//Declaramos un arreglo de objetos
+const estudiantes2 = [
+    {
+        nombre: 'Edison',
+        apellido: 'Lopez',
+        edad: 23,
+        genero: 'Masculino',
+        ciudadNacimiento: 'Quito'
+    },
+    {
+        nombre: 'Maria',
+        apellido: 'Benitez',
+        edad: 24,
+        genero: 'Femenino',
+        ciudadNacimiento: 'Guayaquil'
+    },
+    {
+        nombre: 'Pedro',
+        apellido: 'Alvarez',
+        edad: 40,
+        genero: 'Masculino',
+        ciudadNacimiento: 'Cuenca'
+    },
+    {
+        nombre: 'Luis',
+        apellido: 'Garcia',
+        edad: 35,
+        genero: 'Masculino',
+        ciudadNacimiento: 'Manta'
+    },
+    {
+        nombre: 'Ana',
+        apellido: 'Perez',
+        edad: 28,
+        genero: 'Femenino',
+        ciudadNacimiento: 'Quito'
+    }
+]
+
+console.log(estudiantes2);
+//Imprimir como tabla los datos de los estudiantes
+console.table(estudiantes2);
+
+const eliminarEstudiante = estudiantes2.pop();
+
+console.log(eliminarEstudiante);
+console.table(estudiantes2);
+
+//Desestructuracion de Arrerglos
+const colores = ['Rojo', 'Verde', 'Azul', 'Amarillo', 'Blanco'];
+
+const [c1, c2, c3, c4, c5] = colores;
+console.log(c1);
+console.log(c2);
+
+//Desestructuracion de Arreglos Reducido
+const [m1, m2, m3, m4, m5, m6, m7, m8] = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio'];
+
+console.log(m1);
+console.log(m2);
+console.log(m8);
+
+//La desetruccion puede ser selectiva y traabaja de acorde a la posicion
+const [ , mes2, , , mes5] = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio'];
+
+console.log(mes2);
+console.log(mes5);
+
+//Desestructuracion de Objetos
+const automovil = {
+    marca: 'Toyota',
+    modelo: 'Corolla',
+    anio: 2021,
+    color: 'Rojo'
+}
+
+const {marca, modelo, anio, color} = automovil;
+
+console.log(marca);
+console.log(modelo);
+console.log(anio);
+console.log(color);
+
+const automovil2 = {
+    marca: 'Chevrolet',
+    modelo: 'Spark',
+    anio: 2020,
+    color: 'Blanco'
+}
+
+const desestructuracion = (automovil) =>{
+    
+    console.log(marca);
+    console.log(modelo);
+    console.log(anio);
+    console.log(color);
+}
+
+const desestructuracion2 = ({marca, modelo, anio, color}) =>{
+    
+    console.log(marca);
+    console.log(modelo);
+    console.log(anio);
+    console.log(color);
+}
+
+desestructuracion(automovil2);
+
+//Desestructuracion en una sola linea
+const {animal, raza, edad} = { animal: 'Perro', raza: 'Pastor Aleman', edad: 5, color: 'Cafe'};
+
+console.log(animal);
+console.log(raza);
+console.log(edad);
+
+//Declarar un objeto con un atributro complejo, de tipo objeto
+
+const profesional = {
+    nombre: 'Juan',
+    apellido: 'Perez',
+    edad: 30,
+    genero: 'Masculino',
+    ciudadNacimiento: 'Quito',
+    direccion: {
+        calle: 'Av. 10 de Agosto',
+        numero: 123,
+        barrio: 'La Mariscal'
+    }
+}
+
+//Desetruturar el artibuto de tipo objeto
+
+/*
+const {nombre,direccion} = profesional;
+console.log(nombre);
+console.log(direccion);
+*/
+
+//en dos pasos
+/*
+const {nombre, direccion} = profesional;
+const {calle} = direccion;
+
+console.log(calle);
+*/
+
+//En un paso
+
+const {nombre, direccion: {calle, numero, barrio}} = profesional;
+console.log(nombre);
+console.log(calle);
+console.log(numero);
+console.log(barrio);
